fe443d4a1cfa3cb8516713ca6651196fb4acbc13
dhanushreddy-bizacuity <dhanush.reddy@bizacuity.com>
2025-09-14 12:29:28 +0530

    added notification.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth",authRoute);
 app.use("/api/hospital/",hospitalRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
